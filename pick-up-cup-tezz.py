2c6f5334c5ac391fbbe41148c1db7e3722aaac1f
Tezz <trushar.ghanekar2411@gmail.com>
2026-05-21 14:19:26 +0200

    made changes in pick and place

--- pick-up-cup-tezz.py
+++ pick-up-cup-tezz.py
@@ -1,70 +1,104 @@
+# Software License Agreement (BSD License) 
+# # Copyright (c) 2019, UFACTORY, Inc. 
+# All rights reserved. 
+# # Based on Vinman <vinman.wen@ufactory.cc> <vinman.cub@gmail.com>
 #!/usr/bin/env python3
-# Software License Agreement (BSD License)
-#
-# Copyright (c) 2019, UFACTORY, Inc.
-# All rights reserved.
-#
-# Based on Vinman <vinman.wen@ufactory.cc> <vinman.cub@gmail.com>
 
 import os
 import sys
 import time
-import threading
-sys.path.append(os.path.join(os.path.dirname(__file__), '../../..'))
+
+sys.path.append(os.path.join(os.path.dirname(__file__), "../../.."))
 
 from xarm.wrapper import XArmAPI
 
+
+ROBOT_IP = "192.168.1.204"
+
+# Joint positions
+HOME_POSITION = [0, -53.6, 4.3, 0, 49.4, 0]
+PICK_POSITION = [0, 33, -40, 0, -80, 0]
+LIFT_POSITION = [-22, 25, -72, 0, -45, 0]
+PLACE_POSITION = [-21, 45, -67, 0, -67, 0]
+RETRACT_POSITION = [-21, 16, -67, 0, -42, 0]
+
+# Gripper positions
+GRIPPER_OPEN = 700
+GRIPPER_CLOSE = 520
+
+
 def handle_err_warn_changed(item):
-    print('ErrorCode: {}, WarnCode: {}'.format(item['error_code'], item['warn_code']))
+    print(f"ErrorCode: {item['error_code']}, WarnCode: {item['warn_code']}")
 
-class Jessie(object):
+
+class Jessie:
     def __init__(self):
-        ####
-        # Arm 1
-        ####
-        self.arm = XArmAPI('192.168.1.204', do_not_open=True)
+        self.arm = XArmAPI(ROBOT_IP, do_not_open=True)
         self.arm.register_error_warn_changed_callback(handle_err_warn_changed)
+
+        self.connect_robot()
+
+    def connect_robot(self):
+        print(f"Connecting to robot at {ROBOT_IP}")
+
         self.arm.connect()
-
-        # enable motion
         self.arm.motion_enable(enable=True)
-        # set mode: position control mode
         self.arm.set_mode(0)
-        # set state: sport state
-        self.arm.set_state(state=0)
+        self.arm.set_state(0)
 
-        self.arm1_moving = False
+        time.sleep(1)
+        print("Robot is ready")
 
-    def move_arms_sync(self):
-        arm1_thread = threading.Thread(target=self.move_arm1_traj1)
-        self.arm1_moving = True
-        arm1_thread.start()
-        while self.arm1_moving: #or self.arm2_moving
-            time.sleep(0.1)
+    def move_to(self, name, position, delay=1):
+        print(f"Moving to {name}")
 
-    def move_arm1_traj1(self):
-        self.arm1_moving = True
-        print('Moving arm 1')
-        # self.arm.set_servo_angle(angle=[-180, -47.7, -42.9, 0.0, 90, -355]) # Common Start pose
-        # time.sleep(0.6)
-        gripper_action_code = self.arm.set_gripper_position(790, wait=True)
-        print(f'Gripper action code={gripper_action_code}')
-        time.sleep(2.0)
-        self.arm.set_servo_angle(angle=[-180, 2.4, -93.3, 0.0, 77.5, -355])
-        time.sleep(3.0)
-        self.arm.set_servo_angle(angle=[-180, 11.4, -93.3, 0.6, 77, -355])
-        time.sleep(3.0)
-        gripper_action_code = self.arm.set_gripper_position(100, wait=True)
-        print(f'Gripper action code={gripper_action_code}')
-        time.sleep(3.0)
-        self.arm.set_servo_angle(angle=[-180, 2.4, -93.3, 0.0, 77.5, -355])
-        time.sleep(0.5)
-        self.arm1_moving = False
+        self.arm.set_servo_angle(
+            angle=position,
+            wait=True
+        )
 
-    def disconnect_arms(self):
+        time.sleep(delay)
+
+    def set_gripper(self, name, position, delay=1):
+        print(f"{name}: gripper position {position}")
+
+        self.arm.set_gripper_position(
+            position,
+            wait=True
+        )
+
+        time.sleep(delay)
+
+    def pick_and_place(self):
+        self.move_to("home position", HOME_POSITION)
+
+        self.set_gripper("Opening gripper", GRIPPER_OPEN)
+
+        self.move_to("pick position", PICK_POSITION, delay=3)
+
+        self.set_gripper("Closing gripper", GRIPPER_CLOSE)
+
+        self.move_to("lift position", LIFT_POSITION)
+
+        self.move_to("place position", PLACE_POSITION)
+
+        self.set_gripper("Opening gripper", GRIPPER_OPEN)
+
+        self.move_to("retract position", RETRACT_POSITION)
+
+        self.move_to("home position", HOME_POSITION)
+
+        print("Pick and place movement finished")
+
+    def disconnect_robot(self):
+        print("Disconnecting robot")
         self.arm.disconnect()
 
-if __name__ == '__main__':
+
+if __name__ == "__main__":
     jessie = Jessie()
-    jessie.move_arms_sync()
-    jessie.disconnect_arms()
+
+    try:
+        jessie.pick_and_place()
+    finally:
+        jessie.disconnect_robot()
